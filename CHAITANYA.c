bd652aa94707ac04c2b279beac97a886374eb023
ChaitanyaRaju143 <92910322+ChaitanyaRaju143@users.noreply.github.com>
2021-10-23 12:24:04 +0530

    Add files via upload

---
+++ CHAITANYA.c
@@ -0,0 +1,6 @@
+
+#include<stdio.h>
+void main()
+{
+	printf("C\nH\nA\nI\nT\nA\nN\nY\nA\n");
+}
---
+++ n program chaitanya.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("Roll Number:20P35A03B2\nName:K.Chaitanya Raju\nBranch:Mechanical\nCollege:ACET");
+}
