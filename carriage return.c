3a3b7d250e454ba87cd4e1f70ff10bf68204bb85
ChaitanyaRaju143 <92910322+ChaitanyaRaju143@users.noreply.github.com>
2021-10-27 23:38:21 -0700

    Add files via upload

---
+++ carriage return.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+	printf("chaitanya\rraju\n");
+	printf("vinay\rkumar\n");
+	printf("lakshman\rmaddula");
+}
---
+++ double codes.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("Rich Dad Poor Dad was written by \"Robert Kiyosaki\"");
+}
